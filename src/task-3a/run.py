b0b5390c3e42d52878a7c67bfe5fbb6dc116c820
jiviteshjain <jiviteshjain@outlook.com>
2020-12-15 23:21:53 +0530

    Task-3a: Experiment with Camelot. Extract other tables.

--- src/task-3a/run.py
+++ src/task-3a/run.py
@@ -2,18 +2,21 @@
 import argparse
 import camelot
 import pandas as pd
-
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 150)
+pd.set_option('display.max_colwidth', -1)
 # %%
 
-tables = camelot.read_pdf('../../data/task-3a/EICHERMOT.pdf',
-                          flavor='stream', table_areas=['310,650,595,550'])
+tables = camelot.read_pdf('../../data/task-3a/a6b29367-f3b7-4fb1-a2d0-077477eac1d9.pdf',
+                          flavor='stream', table_areas=['0,650,590,550'], row_tol=100)
 print(tables)
 
 # %%
 display(tables[0].df)
 # %%
 
-plt = camelot.plot(tables[0], kind='text')
+camelot.plot(tables[0], kind='text').show()
 
 
 
@@ -39,3 +42,17 @@ plt = camelot.plot(tables[0], kind='text')
 raise SystemExit
 tables = camelot.read_pdf('../../data/task-3a/EICHERMOT.pdf',
                           flavor='stream', table_areas=['0,570,300,500'], row_tol=14, strip_text='\n')  # Eichermot left
+tables = camelot.read_pdf('../../data/task-3a/EICHERMOT.pdf',
+                          flavor='stream', table_areas=['310,650,595,550'], row_tol=14, strip_text='\n') # Eichermot right
+tables = camelot.read_pdf('../../data/task-3a/EICHERMOT.pdf',
+                          flavor='stream', row_tol=14)[1] # Eichermot bottom requires col spec
+tables = camelot.read_pdf('../../data/task-3a/d9f8e6d9-660b-4505-86f9-952e45ca6da0.pdf',
+                          flavor='lattice') # d9f requires col spec
+tables = camelot.read_pdf('../../data/task-3a/1c1edeee-a13e-4b2e-90be-eb1dd03c3384.pdf',
+                          flavor='lattice') # 1c1 top extra row
+tables = camelot.read_pdf('../../data/task-3a/1c1edeee-a13e-4b2e-90be-eb1dd03c3384.pdf',
+                          flavor='stream', table_areas=['0,480,590,420'], row_tol=14) # 1c1 bottom
+tables = camelot.read_pdf('../../data/task-3a/a6b29367-f3b7-4fb1-a2d0-077477eac1d9.pdf',
+                          flavor='lattice', line_scale=20) # a6b main
+tables = camelot.read_pdf('../../data/task-3a/a6b29367-f3b7-4fb1-a2d0-077477eac1d9.pdf',
+                          flavor='stream', table_areas=['0,650,590,550'], row_tol=100) # a6b address
